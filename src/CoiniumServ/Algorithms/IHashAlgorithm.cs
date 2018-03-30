d2584779a7a8f973227db08c39d9f2239d1f0aeb
John Ahn <jeong760@naver.com>
2018-03-31 08:41:53 +0900

    Update IHashAlgorithm.cs

--- src/CoiniumServ/Algorithms/IHashAlgorithm.cs
+++ src/CoiniumServ/Algorithms/IHashAlgorithm.cs
@@ -4,7 +4,7 @@
 //
 //     CoiniumServ - Crypto Currency Mining Pool Server Software
 //     Copyright (C) 2013 - 2017, CoiniumServ Project
-//     H?eyin Uslu, shalafiraistlin at gmail dot com
+//     Hüseyin Uslu, shalafiraistlin at gmail dot com
 //     https://github.com/bonesoul/CoiniumServ
 // 
 //     Permission is hereby granted, free of charge, to any person obtaining a copy
